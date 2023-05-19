5bb05754577adb7bd71392e17a3e73abd1b1a716
子长 <zichang.nong@tuya.com>
2023-05-20 02:06:58 +0800

    build: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,14 @@ module.exports = {
     commonjs: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-var-requires': 'off',
   },
