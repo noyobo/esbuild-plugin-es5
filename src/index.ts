63b0d0592f3ae10a60819453d5723d59332148c9
noyobo <noyobo@gmail.com>
2023-12-11 09:56:03 +0800

    feat: export transformFile (#34)

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,32 @@
  */
 
 import { OnLoadResult, Plugin } from 'esbuild';
-import { transformFile, Options as SWCOptions } from '@swc/core';
+import { transformFile as _transformFile, Options as SWCOptions } from '@swc/core';
 import deepmerge from 'deepmerge';
 import { convertError } from './convertError';
+
+export function transformFile(file: string, options?: SWCOptions) {
+  const isTs = file.endsWith('.ts') || file.endsWith('.tsx');
+  const isReact = file.endsWith('.jsx') || file.endsWith('.tsx');
+  let transformOptions: SWCOptions = {
+    jsc: {
+      parser: { syntax: isTs ? 'typescript' : 'ecmascript', tsx: isReact },
+      target: 'es5',
+      /**
+       * Use external helpers to avoid duplicate helpers in the output.
+       * esbuild muse has alias `@swc/helpers`
+       */
+      externalHelpers: true,
+    },
+    module: { type: 'es6' },
+    sourceFileName: file,
+    isModule: true,
+  };
+  if (options) {
+    transformOptions = deepmerge(transformOptions, options);
+  }
+  return _transformFile(file, transformOptions);
+}
 
 export function es5Plugin(options?: { filter?: RegExp; swc?: SWCOptions }) {
   return {
@@ -17,34 +40,15 @@ export function es5Plugin(options?: { filter?: RegExp; swc?: SWCOptions }) {
       const enableSourcemap = !!buildOptions.sourcemap;
 
       build.onLoad({ filter: options?.filter || /\.([tj]sx?|mjs)$/ }, args => {
-        const isTs = args.path.endsWith('.ts') || args.path.endsWith('.tsx');
-        const isReact = args.path.endsWith('.jsx') || args.path.endsWith('.tsx');
-
-        let transformOptions: SWCOptions = {
-          jsc: {
-            parser: { syntax: isTs ? 'typescript' : 'ecmascript', tsx: isReact },
-            target: 'es5',
-            /**
-             * Use external helpers to avoid duplicate helpers in the output.
-             * esbuild muse has alias `@swc/helpers`
-             */
-            externalHelpers: true,
-          },
-          module: { type: 'es6' },
-          /**
-           * Generate inline source maps to enable esbuild to properly handle sourcemaps.
-           */
-          sourceMaps: enableSourcemap ? 'inline' : false,
-          sourceFileName: args.path,
-          isModule: true,
-        };
-
-        if (options?.swc) {
-          transformOptions = deepmerge(transformOptions, options?.swc);
-        }
-
         return new Promise<OnLoadResult>(resolve => {
-          transformFile(args.path, transformOptions)
+          const opts = deepmerge<SWCOptions>(options?.swc || {}, {
+            /**
+             * Generate inline source maps to enable esbuild to properly handle sourcemaps.
+             */
+            sourceMaps: enableSourcemap ? 'inline' : false,
+          });
+
+          transformFile(args.path, opts)
             .then(({ code }) => {
               resolve({ contents: code, loader: 'js' });
             })
