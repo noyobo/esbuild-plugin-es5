c5703a1f2015cb9f1e622d22e9f524699f45de90
noyobo <noyobo@gmail.com>
2025-03-04 00:06:18 +0800

    ci: rm eslint

--- src/convertError.ts
+++ src/convertError.ts
@@ -1,6 +1,5 @@
 import { PartialMessage } from 'esbuild';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const convertError = (error: any): PartialMessage => {
   return {
     pluginName: 'esbuild-plugin-es5',
